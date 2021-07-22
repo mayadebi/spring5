d511a7844f991b6fbab5fc85ac92d307041b3a58
mayadebi <503991583@qq.com>
2021-07-22 23:07:44 +0800

    整合mybatis方式二,不需要在实现类里面注入sqlsession,改为继承一个类，在xml里面注入sqlsession

--- spring-11/src/main/java/com/fzy/dao/SysUserMapperImpl.java
+++ spring-11/src/main/java/com/fzy/dao/SysUserMapperImpl.java
@@ -2,18 +2,16 @@ package com.fzy.dao;
 
 import com.fzy.entity.SysUser;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class SysUserMapperImpl implements SysUserMapper{
-    @Autowired
-    private SqlSessionTemplate sqlSession;
-
+public class SysUserMapperImpl extends SqlSessionDaoSupport implements SysUserMapper{
     public List<SysUser> getUserListAll(){
-        SysUserMapper mapper = sqlSession.getMapper(SysUserMapper.class);
+        SysUserMapper mapper = getSqlSession().getMapper(SysUserMapper.class);
         return mapper.getUserListAll();
     }
 }
